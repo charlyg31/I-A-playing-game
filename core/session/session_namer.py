465543f69b91abd551b92645ea6b8121a7dead29
charlyg31 <secour123456@hotmail.fr>
2025-04-01 14:18:28 +0200

    Delete core directory

--- core/session/session_namer.py
+++
@@ -1,8 +0,0 @@
-import random
-
-class SessionNamer:
-    def __init__(self):
-        self.themes = ["Deck", "AI", "Mind", "Nova", "Strat", "Shadow", "Yami", "Core", "Bot"]
-
-    def generate_name(self):
-        return random.choice(self.themes) + random.choice(self.themes)
